b956ea9af8f165c47150d5ce95241d66970843f9
Andrew Plonski <agplonski@hotmail.com>
2012-10-16 00:29:12 -0500

    changes and changes

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-# Initialize the rails application
-Sample::Application.initialize!
+ #Initialize the rails application
+Blog::Application.initialize!  
 
-ActiveRecord::Base.include_root_in_json = true 
-
-config.active_support.deprecation = :log
+ 
